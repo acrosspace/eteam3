2ecbccfea51ff7c9877120996d066180946128f8
cass <casswoo@gmail.com>
2024-07-06 10:21:21 +0900

    wip

--- test2.js
+++ test2.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
 document.addEventListener("DOMContentLoaded", () => {
   var width = window.innerWidth;
   var height = window.innerHeight;
@@ -14,10 +16,69 @@ document.addEventListener("DOMContentLoaded", () => {
   camera.position.set(0, 5, 5);
   camera.lookAt(0, 0, 0);
 
+  const girdSize = 10;
+  const gridDivisions = 10;
+  const gridHelper = new THREE.GridHelper(girdSize, gridDivisions);
+  scene.add(gridHelper);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+
   var geometry1 = new THREE.BoxGeometry(1, 1, 1);
   var material1 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
   var cube1 = new THREE.Mesh(geometry1, material1);
   scene.add(cube1);
 
-  renderer.render(scene, camera);
+  // Coins
+  const coinGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.1, 100);
+  const coinMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+
+  const coins = [];
+  let collectedCoins = 0;
+  for (let i = 0; i < 10; i++) {
+    const coin = new THREE.Mesh(coinGeometry, coinMaterial);
+    coin.position.set(Math.random() * 10 - 5, 0.5, Math.random() * 10 - 5);
+    coin.rotation.x = Math.PI / 2;
+    // make rotation z random between 0 and 360 degrees
+    coin.rotation.z = Math.random() * Math.PI * 2;
+    scene.add(coin);
+    coins.push(coin);
+  }
+
+  const coinCountElement = document.getElementById("coin-count");
+  coinCountElement.innerHTML = `coins: ${collectedCoins}`;
+
+  window.addEventListener("keydown", (event) => {
+    console.log(event.key);
+
+    if (event.key == "w") {
+      cube1.position.z = cube1.position.z + 1;
+    }
+  });
+  window.addEventListener("keyup", (event) => {
+    console.log(event.key);
+  });
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    //cube1.rotation.x += 0.01;
+
+    // Collision detection
+    const playerBox = new THREE.Box3().setFromObject(cube1);
+
+    for (let i = 0; i < coins.length; i++) {
+      const coinBox = new THREE.Box3().setFromObject(coins[i]);
+      if (playerBox.intersectsBox(coinBox)) {
+        scene.remove(coins[i]);
+        coins.splice(i, 1);
+        i--; // Adjust index after removal
+
+        console.log(conis.length);
+      }
+    }
+
+    renderer.render(scene, camera);
+  }
+
+  animate();
 });
